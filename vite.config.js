9edd9e7ce9509c2c3e465b04e39c3a1fe32fd28e
Olga Brendak <olga.brendak.dg@gmail.com>
2025-06-26 15:58:27 +0300

    add for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  base: '/rental-app/',
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
